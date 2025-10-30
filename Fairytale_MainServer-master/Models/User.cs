67ea33eb5c3f7a5a4804e74fd679b02abd7f3931
km7624 <51872595+km7624@users.noreply.github.com>
2025-10-30 20:22:30 +0900

    Add files via upload

---
+++ Fairytale_MainServer-master/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace MainServer.Models
+{
+    public class User
+    {
+        public string Id { get; set; } // 구글 계정 연동
+        public string DeviceId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastLoginAt { get; set; }
+        public List<Story> Stories { get; set; }
+        public List<UserTag> UserTags { get; set; }
+        public List<Recommendation> Recommendations { get; set; }
+
+        //10-30 임강묵 추가
+        public string? ReferenceVoiceFileId { get; set; }
+    }
+}
